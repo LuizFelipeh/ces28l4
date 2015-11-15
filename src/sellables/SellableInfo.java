70a30b2de72db1b528f6d1e8b7309880eaf80368
Ghust1995 <gustavo.ceci95@gmail.com>
2015-11-15 13:29:37 -0200

    Sellable Package Mostly Complete

--- src/sellables/SellableInfo.java
+++ src/sellables/SellableInfo.java
@@ -6,12 +6,37 @@ import taxes.TributaryCategory;
  * Created by Felipeh on 11/11/2015.
  */
 public class SellableInfo {
-    private TributaryCategory _tributaryCategory;
-    private float _price;
-    private String _name;
-    private SellableClass _sellableClass;
+    private final long _id;
+    private final TributaryCategory _tributaryCategory;
+    private final float _price;
+    private final String _name;
+    private final SellableClass _sellableClass;
+
+    public SellableInfo(long id, TributaryCategory tributaryCategory, float price, String name, SellableClass sellableClass) {
+        _id = id;
+        _tributaryCategory = tributaryCategory;
+        _price = price;
+        _name = name;
+        _sellableClass = sellableClass;
+    }
 
     public SellableClass getSellableClass() {
         return _sellableClass;
+    }
+
+    public TributaryCategory getTributaryCategory() {
+        return _tributaryCategory;
+    }
+
+    public float getPrice() {
+        return _price;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public long getId() {
+        return _id;
     }
 }
