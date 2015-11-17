9bf176957faa0ec23b21addf30942f23495b68dd
Luiz Felipeh <luizfelipeh95@gmail.com>
2015-11-17 15:34:49 -0200

    Minor changes in taxes package

--- src/taxes/Tax.java
+++ src/taxes/Tax.java
@@ -7,26 +7,43 @@ import java.util.List;
  * Created by Felipeh on 11/11/2015.
  */
 public class Tax {
-    private java.util.List<TaxRule> _rules;
+    private String _taxName;
+    private List<TaxRule> _rules;
     private static TaxRule defaultTaxRule;
     private boolean _usingDefault = true;
 
-    public Tax()
+    public Tax(String name)
     {
         defaultTaxRule = new DefaultTaxRule();
+        _taxName = name;
     }
 
 
     public void addRule(TaxRule rule) {
+        _rules.add(rule);
     }
 
     public float calculate(TaxData taxData) {
-        return 0;
+        float taxValue = 0;
+        for (TaxRule rule:_rules){
+            taxValue += rule.calculate(taxData);
+        }
+
+        if(_usingDefault) taxValue += defaultTaxRule.calculate(taxData);
+
+        return taxValue;
     }
 
     public void useDefault(boolean value) {
+        _usingDefault = value;
     }
 
     public static void setDefault(TaxRule taxRule) {
+        defaultTaxRule = taxRule;
     }
+
+    public String getTaxName() {
+        return _taxName;
+    }
+
 }
--- src/taxes/TaxHistoryObject.java
+++ src/taxes/TaxHistoryObject.java
@@ -1,10 +1,30 @@
 package taxes;
 
+import java.util.Date;
+
 /**
  * Created by Felipeh on 13/11/2015.
  */
 public class TaxHistoryObject {
     private float _total;
-    private java.util.Date _submissionDate;
+    private Date _submissionDate;
     private float _taxValue;
+
+    public float getTotal() {
+        return _total;
+    }
+
+    public float getTaxValue() {
+        return _taxValue;
+    }
+
+    public Date getSubmissionDate() {
+        return _submissionDate;
+    }
+
+    public TaxHistoryObject(float total, Date submissionDate, float taxValue){
+        _total = total;
+        _submissionDate = submissionDate;
+        _taxValue = taxValue;
+    }
 }
